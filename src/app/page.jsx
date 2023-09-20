0a798acda349faf6aa4c8958900b9681560fcc6f
Manuel Takata <takata211011@gmail.com>
2023-09-19 20:27:01 -0500

    remove bg image

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,7 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hello
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+      example!
     </main>
   )
 }
